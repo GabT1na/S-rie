916ad3b15cf9ccf44bfce3314cc310d1bcd52745
Gabrielly Cristina <167761882+GabT1na@users.noreply.github.com>
2024-05-13 16:32:20 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("*****************");
+    System.out.println("");
+    System.out.println("Série: Brooklyn Nine-Nine");
+    System.out.println(" ");
+    System.out.println("Ano de Lançamento:17 de setembro de 2013");
+    System.out.println(" ");
+    System.out.println("Plataforma de Stream: Netflix");
+    System.out.println(" ");
+    System.out.println("Possui 8 temporadas");
+    System.out.println(" ");
+    System.out.println("Gêneros: Sitcoms,Comédias para a TV,Séries policiais,Séries dos EUA");
+    System.out.println(" ");
+    
+    System.out.println("*****************");
+    System.out.println(" ");
+    System.out.println("SINOPSE");
+    System.out.println(" ");
+    System.out.println("*****************");
+    System.out.println(" ");
+    
+    System.out.println("Situado na 99.ª delegacia fictícia do Departamento de Polícia de Nova York, no Brooklyn, Brooklyn Nine-Nine segue uma equipe de detetives liderados pelo capitão Raymond Holt (Andre Braugher), recém-nomeado e muito sério. Os detetives incluem Jake Peralta (Andy Samberg), que tem uma alta taxa de prisões bem-sucedidas e casos resolvidos, apesar de sua atitude relaxada, despreocupada e (às vezes) infantil. Ele finalmente se apaixona por sua parceira severa, nerd, amável, mas adorável, Amy Santiago (Melissa Fumero). O esforçado, mas tímido Charles Boyle (Joe Lo Truglio) faz parceria com a estóica, agressiva e severa Rosa Diaz (Stephanie Beatriz). Os dois detetives mais velhos, Michael Hitchcock (Dirk Blocker) e Norm Scully (Joel McKinnon Miller) muitas vezes parecem incompetentes, estúpidos e preguiçosos, mas resolveram mais casos ao longo dos anos simplesmente porque estão trabalhando há mais tempo. Os detetives se reportam ao sargento Terry Jeffords (Terry Crews), um gigante gentil e devotado homem de família que inicialmente tem uma fobia de voltar ao trabalho policial de campo ativo por medo de ser morto no cumprimento do dever e deixar suas filhas sem pai. A delegacia também inclui a administradora civil sarcástica e dominadora Gina Linetti (Chelsea Peretti), que não gosta de seu trabalho, prioriza suas mídias sociais e acredita que dançar é seu objetivo de vida.");
+    System.out.println(" ");
+    
+    System.out.println("A série também é conhecida por um episódio que utilizou uma música dos Backstreets Boys e acabou estourando na mídia");
+    System.out.println(" ");
+    
+    System.out.println("*****************");
+    System.out.println(" ");
+    System.out.println("Link do Trailer: https://youtu.be/icTOP9F17pU?si=G0yIy1MCGczbZcWn");
+  }
+}
